07f812849e34585a1a19a2b62796eb83cf9f861f
Tony Bellomo <CoderTonyB@gmail.com>
2020-05-09 17:15:55 -0400

    can start and stop stream now

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 var getUserMedia = require('get-user-media-promise');
 var MicrophoneStream = require('microphone-stream');
 
+var micStream;
+
 document.getElementById('my-start-button').onclick = function () {
   // note: for iOS Safari, the constructor must be called in response to a tap, or else the AudioContext will remain
   // suspended and will not provide any audio data.
   var micStream = new MicrophoneStream();
-
-  getUserMedia({ video: false, audio: true })
-    .then(function (stream) {
-      micStream.setStream(stream);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 
   // get Buffers (Essentially a Uint8Array DataView of the same Float32 values)
   micStream.on('data', function (chunk) {
@@ -20,20 +14,23 @@ document.getElementById('my-start-button').onclick = function () {
     // (This actually just creates a new DataView - the underlying audio data is not copied or modified.)
     var raw = MicrophoneStream.toRaw(chunk);
     //...
-
+    console.log(raw);
     // note: if you set options.objectMode=true, the `data` event will output AudioBuffers instead of Buffers
-  });
-
-  // or pipe it to another stream
-  micStream.pipe(/*...*/);
-
-  // It also emits a format event with various details (frequency, channels, etc)
-  micStream.on('format', function (format) {
-    console.log(format);
   });
 
   // Stop when ready
   document.getElementById('my-stop-button').onclick = function () {
     micStream.stop();
+    console.log('stop');
+    console.log(micStream);
   };
+
+  getUserMedia({ video: false, audio: true })
+    .then(function (stream) {
+      micStream.setStream(stream);
+      console.log('streaming...');
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
